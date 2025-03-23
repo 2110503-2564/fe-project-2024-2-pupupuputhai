f7ef8ed13bf5e2354dcbe8d1df83ec4d35a198c1
Tripob Pongpanich <taiz.7149@gmail.com>
2025-03-23 15:30:47 +0700

    add form component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,14 @@
 import TopMenu from "@/components/TopMenu";
+import CreateRestaurant from "@/components/form/CreateRestaurant";
+import EditRestaurant from "@/components/form/EditRestaurant copy";
 
 export default function MainTopBar({children} : {children:React.ReactNode}){
     return (
         <div>
             <TopMenu/>
             <div className="h-[60px]"></div>
-
-            {children}
+            <EditRestaurant/>
+            {/* {children} */}
         </div>
     )
 }
